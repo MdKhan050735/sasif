a7b305dc3a11d0c8a4fda542d1226d72746c6d4d
MdKhan050735 <142714060+MdKhan050735@users.noreply.github.com>
2024-09-24 23:34:48 -0700

    Add files via upload

---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+# LECTURE -7
+#FILE I/O IN PYTHON
+#PYTHON CAN BE USED TO  PERFORM OPERATION ON  A FILE . (READ AND WRITE)
+
+with open("prac.txt","w"): as f:
+f.write("hi everyone \n we are learnig Python\n")
+f.write("Using Java .\n I like programming in java\n")
+    
